c6fbc6a57fd4b960bc56f349837cb125a2aed02e
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-07-24 10:45:09 -0500

    Webpack - Running the bundler

--- src/index.js
+++ src/index.js
@@ -8,3 +8,16 @@ function main() {
 }
 
 $(main);
+
+$(document).ready(function() {
+  shoppingList.bindEventListeners();
+
+  // On initial load, fetch Shopping Items and render
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    })
+    .catch(err => console.log(err.message));
+});
+
